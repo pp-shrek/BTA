a500bb336d8ba9ac93c592f6d9774ff507d26bfe
pp-shrek <153538284+pp-shrek@users.noreply.github.com>
2024-02-05 16:14:10 +0200

    day28

---
+++ React/react-basics/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <h1>Assignment 1: Simple props</h1>
+      <h1>Assignment 2</h1>
+      <h1>Assignment 3</h1>
+      <h1>Assignment 4</h1>
+    </>
+  )
+}
+
+export default App
